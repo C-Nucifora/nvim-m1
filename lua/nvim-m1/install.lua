d35306f274ba4cc22b801ac71ed86b37f89e3245
Ned Lane <nedlane06@icloud.com>
2026-08-21 00:11:49 +1000

    chore: refresh formatter and linter pins

--- lua/nvim-m1/install.lua
+++ lua/nvim-m1/install.lua
@@ -30,8 +30,8 @@ local M = {}
 --- m1-vscode's `package.json` `serverVersion` pin.
 M.versions = {
   ["m1-lsp"] = "v0.50.2",
-  ["m1-fmt"] = "v0.17.0",
-  ["m1-lint"] = "v0.24.0",
+  ["m1-fmt"] = "v0.17.1",
+  ["m1-lint"] = "v0.24.1",
   ["m1-project"] = "v0.14.0",
 }
 
